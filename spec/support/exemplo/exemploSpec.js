f54ae1d8e3be571bd8375092879415e4c16a786a
Ketlin de lima <ketlin@senocwb.com>
2023-09-29 11:25:35 -0300

    'files'

---
+++ spec/support/exemplo/exemploSpec.js
@@ -0,0 +1,9 @@
+describe("Suite de teste de exemplo com NodeJS", function(){
+    it("Deve demonstrar o uso do Jasmine com o NodeJS", function(){
+        expect(true).toEqual(true);
+    });
+
+    it("Deve demonstrar o uso simultâneo do Karma", function(){
+        expect(1).toEqual(1);
+    });
+});
